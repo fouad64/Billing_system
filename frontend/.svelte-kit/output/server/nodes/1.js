3cbc81d0f0cea7d9f694a0762de567f1c4c6dce5
Ziad-Khattab <209370232+Ziad-Khattab@users.noreply.github.com>
2026-04-27 14:16:53 +0300

    feat: complete standalone JAR integration with Nginx

--- frontend/.svelte-kit/output/server/nodes/1.js
+++ frontend/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.XJ1vWcl9.js","_app/immutable/chunks/Cng9lrDC.js","_app/immutable/chunks/CafSqgsp.js","_app/immutable/chunks/CIJMg2B1.js","_app/immutable/chunks/B-1EDjby.js","_app/immutable/chunks/DOI6MEZn.js","_app/immutable/chunks/CEqC-9dc.js","_app/immutable/chunks/BZvePH1H.js"];
+export const imports = ["_app/immutable/nodes/1.Dp6EJtQp.js","_app/immutable/chunks/Cng9lrDC.js","_app/immutable/chunks/CafSqgsp.js","_app/immutable/chunks/uE8DdJig.js","_app/immutable/chunks/DS9Quw7Y.js","_app/immutable/chunks/DOI6MEZn.js","_app/immutable/chunks/CEqC-9dc.js","_app/immutable/chunks/CDeecSPS.js"];
 export const stylesheets = [];
 export const fonts = [];
---
+++ src/main/webapp/_app/immutable/entry/start.DBHj-_qA.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CDeecSPS.js";import{x as t}from"../chunks/DS9Quw7Y.js";export{t as load_css,r as start};
