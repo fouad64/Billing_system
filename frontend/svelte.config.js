1db38c4ac27e469fe99c6ab01ffedd665b0fbf9c
mohamed-abdulnaby <mohamed_abdulnaby@icloud.com>
2026-04-27 14:09:38 +0300

    edit: edited svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,16 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
-const isProd = process.env.VERCEL === '1';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages:       isProd ? 'build' : '../src/main/webapp',
-			assets:      isProd ? 'build' : '../src/main/webapp',
-			fallback:    'index.html',
-			precompress: false,
-			strict:      true
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: ""
