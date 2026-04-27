8d41893c2ddb86ed196f5ed2ca24b460827dc82a
Ziad-Khattab <209370232+Ziad-Khattab@users.noreply.github.com>
2026-04-27 04:06:39 +0300

    chore: Clean up DB comments and update built assets.

--- frontend/.svelte-kit/output/server/nodes/1.js
+++ frontend/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CakRxpLt.js","_app/immutable/chunks/Cng9lrDC.js","_app/immutable/chunks/CafSqgsp.js","_app/immutable/chunks/UsDbBZuo.js","_app/immutable/chunks/Bhbr1Ggr.js","_app/immutable/chunks/DOI6MEZn.js","_app/immutable/chunks/CEqC-9dc.js","_app/immutable/chunks/7CDtYSgz.js"];
+export const imports = ["_app/immutable/nodes/1.DQX2R2jl.js","_app/immutable/chunks/Cng9lrDC.js","_app/immutable/chunks/CafSqgsp.js","_app/immutable/chunks/C4MF6-0A.js","_app/immutable/chunks/a0sD2_6o.js","_app/immutable/chunks/DOI6MEZn.js","_app/immutable/chunks/CEqC-9dc.js","_app/immutable/chunks/2DZDIbpT.js"];
 export const stylesheets = [];
 export const fonts = [];
