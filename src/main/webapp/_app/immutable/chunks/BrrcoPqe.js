52357795f24f428e42a356f7c3957255be764f8a
Ziad-Khattab <209370232+Ziad-Khattab@users.noreply.github.com>
2026-04-29 19:35:59 +0300

    fix: robust path detection for CDR import across environments

---
+++ src/main/webapp/_app/immutable/chunks/BrrcoPqe.js
@@ -0,0 +1,1 @@
+import{p as r}from"./mnU8URWv.js";import{s as t}from"./C0VsL41Y.js";const o={get error(){return r.error},get status(){return r.status}};t.updated.check;export{o as p};
---
+++ src/main/webapp/_app/immutable/entry/start.B__A5Ehk.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/C0VsL41Y.js";import{x as t}from"../chunks/mnU8URWv.js";export{t as load_css,r as start};
