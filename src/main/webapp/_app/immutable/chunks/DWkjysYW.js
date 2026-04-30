da1232fd91a9fd88891dad98e61bc06fff4ce8cd
Ziad-Khattab <209370232+Ziad-Khattab@users.noreply.github.com>
2026-04-30 13:26:23 +0300

    Fix: Restore Jasper 7.0.6, transformers, and Long data types for usage fields

--- src/main/webapp/_app/immutable/chunks/DWkjysYW.js
+++
@@ -1,1 +0,0 @@
-import{p as r}from"./k6yvPQa-.js";import{s as t}from"./DQ-osOeS.js";const o={get error(){return r.error},get status(){return r.status}};t.updated.check;export{o as p};
--- src/main/webapp/_app/immutable/entry/start.ChbcCOFs.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DQ-osOeS.js";import{x as t}from"../chunks/k6yvPQa-.js";export{t as load_css,r as start};
