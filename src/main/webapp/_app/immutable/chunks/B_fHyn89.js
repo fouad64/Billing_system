f96cd902fc0c546f6414bbef4f533d137863e07d
Ziad-Khattab <209370232+Ziad-Khattab@users.noreply.github.com>
2026-04-30 15:17:47 +0300

    Cleanup: Removed legacy branding assets and redundant picture files

---
+++ src/main/webapp/_app/immutable/chunks/B_fHyn89.js
@@ -0,0 +1,1 @@
+import{p as r}from"./CsaXExZJ.js";import{s as t}from"./Bg10t0VQ.js";const o={get error(){return r.error},get status(){return r.status}};t.updated.check;export{o as p};
---
+++ src/main/webapp/_app/immutable/entry/start.Dk-a5UsB.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Bg10t0VQ.js";import{x as t}from"../chunks/CsaXExZJ.js";export{t as load_css,r as start};
